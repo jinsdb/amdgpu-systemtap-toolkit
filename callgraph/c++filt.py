7822a5118b675cccbaa1c2664051dffb8ada579f
Qu Huang <huangqu@Hygon.cn>
2021-10-23 16:52:31 +0800

    update

--- callgraph/c++filt.py
+++ callgraph/c++filt.py
@@ -16,12 +16,11 @@ f = open("/tmp/tmp2.dot", "r")
 lines = f.readlines()
 for line in lines:
     line = line.strip("\n")
-    #pattern=re.sub(u"\\<.*\\>", "", line)
-    #print("%s" % pattern)
     str = line.split('->')
     if (len(str) < 2):
-       print(line)
-       continue
+        pattern=re.sub(u"\\<.*\\>", "", line)
+        print("%s" % pattern)
+        continue
     text1 = re.sub(u"\\<.*\\>", "", str[0])
     text2 = re.sub(u"\\<.*\\>", "", str[1])
     print("%s -> %s"%(text1, text2))    
